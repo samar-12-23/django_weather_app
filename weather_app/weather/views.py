09c89b3c260d06d34fcc2103fea9860e8e8e56b9
Samar Gupta <sg4758101@gmail.com>
2026-05-13 13:59:27 +0530

    modification done

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
+import json
+import urllib.request
 
 def index(request):
+    city = ''
+    data = {}
     
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        city = request.POST.get('city', '')
+        if city:
+            res = urllib.request.urlopen(
+                'http://api.openweathermap.org/data/2.5/weather?q=' + city +
+                '&appid=d04bac7541500076428d634567772c7e'
+            ).read()
+            json_data = json.loads(res)
+            data = {
+                "country_code": str(json_data['sys']['country']),
+                "coordinate": str(json_data['coord']['lon']) + ' ' +
+                              str(json_data['coord']['lat']),
+                "temp": str(json_data['main']['temp']) + 'k',
+                "pressure": str(json_data['main']['pressure']),
+                "humidity": str(json_data['main']['humidity']),
+            }
+
+    return render(request, 'index.html', {'city': city, 'data': data})
