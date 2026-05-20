e8229bd9be50cd2872371c0f9eb8fda0c840b3ff
Samar Gupta <sg4758101@gmail.com>
2026-05-20 11:25:15 +0530

    updating the code

--- weather_app/weather_app/settings.py
+++ weather_app/weather_app/settings.py
@@ -22,8 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-bv(6unc9to6*z2)g5e+sf4z-5%)hu%g(fbxx%igrgbk@7+h%1('
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+MY_API_KEY=os.getenv('THIRD_PARTY_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
