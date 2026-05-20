fa3dea7863cb5ad327d9bb95abeed69574dbdb42
Samar Gupta <sg4758101@gmail.com>
2026-05-20 11:47:06 +0530

    done

--- weather_app/weather_app/settings.py
+++ weather_app/weather_app/settings.py
@@ -14,7 +14,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-OPEN_WEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
+OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
 
 from pathlib import Path
 
